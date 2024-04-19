9ec40da603512b161f62beae82ff3b6a3dbf684a
athira <athiraomanakuttan20@gmail.com>
2024-04-19 19:16:58 +0530

    Design part compleated

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -9,7 +9,7 @@ const loginCred = { userName: "Athira", password: "123456" };
 // --------------------------- Routes ------------------------------
 
 router.get("/", (req, res) => {
-  res.render("loginPage");
+  res.render("loginPage",{error : null});
 });
 router.post("/login", (req, res) => {
   const userCred = req.body;
@@ -19,7 +19,7 @@ router.post("/login", (req, res) => {
   )
     res.render("homePage", { userName: loginCred.userName });
   else
-    res.render("loginPage",{error : "Sorry! UserName or Password Incorrect "});
+    res.render("loginPage",{error : "Error : Username or Password Incorrect !"});
 });
 router.get("/login", (req, res) => {
   res.redirect("/");
